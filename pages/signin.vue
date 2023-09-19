db9c1920af1703acee87801d33bd1ade929dbfb4
AlexLawKas <AlexLawKas@yandex.ru>
2023-09-19 23:20:01 +0400

    1

--- pages/signin.vue
+++ pages/signin.vue
@@ -5,8 +5,10 @@
           <h1 class="h3 mb-3 mt-3 font-weight-normal">Пожалуйста укажите Email и пароль</h1>
           <label for="inputEmail" class="sr-only">Email пользователя</label>
           <input id="inputEmail" class="form-control" placeholder="Email пользователя" required="" v-model="login.email">
+          <br>
           <label for="inputPassword" class="sr-only">Пароль</label>
           <input type="password" id="inputPassword" class="form-control mt-2" placeholder="Пароль" required="" v-model="login.password">
+          <br>
           <button class="btn mt-2 btn-lg btn-primary btn-block" type="submit">Войти</button>
         </form>
       </div>
