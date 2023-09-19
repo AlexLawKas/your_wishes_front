1108cc544511ad463042acc92f43d3d01629e6d2
AlexLawKas <AlexLawKas@yandex.ru>
2023-09-19 23:29:33 +0400

    2

--- pages/signin.vue
+++ pages/signin.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-      <div class="text-center">
-        <form class="form-signin" @submit.prevent="userLogin">
+      <div class="text-center" style='overflow-y:hidden;'>
+        <form class="form-signin" @submit.prevent="userLogin" style='overflow-y:hidden;'>
           <h1 class="h3 mb-3 mt-3 font-weight-normal">Пожалуйста укажите Email и пароль</h1>
           <label for="inputEmail" class="sr-only">Email пользователя</label>
           <input id="inputEmail" class="form-control" placeholder="Email пользователя" required="" v-model="login.email">
